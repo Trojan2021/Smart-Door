7824449d231c5a7ac362e61388d23a182458323c
Trojan2021 <sawyereramsey@gmail.com>
2022-02-14 15:27:22 -0500

    Face Encoder

--- Face_Encoder.py
+++ Face_Encoder.py
@@ -1,7 +1,7 @@
 import face_recognition
 import numpy as np
 
-bret_image = face_recognition.load_image_file("Hayden.jpg")
-bret_face_encoding = face_recognition.face_encodings(bret_image)[0]
+image = face_recognition.load_image_file("Hayden.jpg")
+face_encoding = face_recognition.face_encodings(image)[0]
 
-np.savetxt("Hayden_Encoding.txt", bret_face_encoding, delimiter = ", ")
+np.savetxt("Hayden_Encoding.txt", face_encoding, delimiter = ", ")
