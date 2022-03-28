1271ccbe21e52644baa55d8c261e5ab47594ad97
Sawyer Ramsey <sawyereramsey@gmail.com>
2022-03-27 22:03:52 -0400

    Ability To Take Pictures

--- Face_Encoder.py
+++ Face_Encoder.py
@@ -1,9 +1,17 @@
 #File to Encode Faces
 
+import os
 import tkinter as tk
+from sys import platform
 from tkinter import filedialog
-import numpy as np
+
+import cv2
 import face_recognition
+import numpy as np
+from PIL import Image, ImageTk
+
+global cameraOn
+cameraOn = False
 
 filename = 0
 def Beans():
@@ -16,11 +24,77 @@ def Beans():
     source.insert(0,filename)
 
 def CompArray():
-    image = face_recognition.load_image_file(source.get())
-    face_encoding = face_recognition.face_encodings(image)[0]
-    np.savetxt(("Encodings/" + name.get()), face_encoding, delimiter = ", ")
+    if source.get() == "From Picture":
+        image = face_recognition.load_image_file("Media/tempfile.png")
+        face_encoding = face_recognition.face_encodings(image)[0]
+        np.savetxt(("Encodings/" + name.get()), face_encoding, delimiter = ", ")
+    else:
+        image = face_recognition.load_image_file(source.get())
+        face_encoding = face_recognition.face_encodings(image)[0]
+        np.savetxt(("Encodings/" + name.get()), face_encoding, delimiter = ", ")
+    if os.path.exists("Media/tempfile.png"):
+      os.remove("Media/tempfile.png")
+
+def takePicture():
+
+    global cameraOn
+    global co
+
+    if not cameraOn:
+
+        cameraOn = True
+        camera['text'] = "Take Picture"
+
+        # Video capture
+        if platform == "linux":
+            video_capture = cv2.VideoCapture(0)
+        elif platform == "win32":
+            video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+        while cameraOn:
+
+            # Reading in the video
+            ret, frame = video_capture.read()
+
+            # Displaying the camera
+            blue,green,red = cv2.split(frame)
+            c = cv2.merge((red,green,blue))
+            co = Image.fromarray(c)
+            coo = ImageTk.PhotoImage(image=co)
+            picture.configure(image=coo)
+            window.update()
+
+
+        cv2.imwrite("Media/tempfile.png", frame)
+
+        video_capture.release()
+
+
+    else:
+        cameraOn = False
+        source.delete(0,"end")
+        source.insert(0,"From Picture")
+
+
+
+
+
 
 window = tk.Tk()
+
+global img
+img = ImageTk.PhotoImage(Image.open('Media/logo_with_text.jpg'))
+
+picture = tk.Label(
+    image=img
+)
+picture.pack()
+
+camera = tk.Button(
+    text="Open Camera",
+    command=takePicture
+)
+camera.pack()
 
 bean = tk.Label(text="Please seclect the picture with a\n face that you would like to encode\n(*.jpg only)")
 bean.pack()
